23de4022546a86c1cf8816ad87c67edfd6698dae
liushoukun <liushoukun66@gmail.com>
2024-03-21 00:03:14 +0800

    feat(商品): 类目优化

---
+++ lang/zh_CN/product-property-group.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'labels'  => [
+        'ProductPropertyGroup'   => '属性组',
+        'product-property-group' => '属性组',
+    ],
+    'fields'  => [
+        'name'         => '名称',
+        'sort'         => '排序',
+        'status'       => '状态',
+        'extends'      => '扩展字段',
+        'creator_type' => '创建者类型',
+        'creator_id'   => '创建者UID',
+        'updater_type' => '更新者类型',
+        'updater_id'   => '更新者UID',
+    ],
+    'options' => [
+    ],
+];
