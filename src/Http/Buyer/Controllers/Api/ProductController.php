65082b55618ccb35975d552c3aaf3263a59cb0d5
liushoukun <liushoukun66@gmail.com>
2024-03-27 00:46:52 +0800

    refactor: 服务重构

--- src/Http/Buyer/Controllers/Api/ProductController.php
+++ src/Http/Buyer/Controllers/Api/ProductController.php
@@ -4,7 +4,8 @@ namespace RedJasmine\Product\Http\Buyer\Controllers\Api;
 
 
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use RedJasmine\Product\Business\Buyer\ProductService;
+
+use RedJasmine\Product\Application\Buyer\Services\ProductService;
 use RedJasmine\Product\Http\Buyer\Resources\ProductResource;
 
 class ProductController extends Controller
@@ -15,13 +16,15 @@ class ProductController extends Controller
 
     public function index() : AnonymousResourceCollection
     {
-
-        $result = $this->service->query()->paginate();
+        // TODO 需要设置实例为
+        $this->service->query->setIncludes(null);
+        $result = $this->service->query(true)->paginate();
         return ProductResource::collection($result);
     }
 
     public function show($id) : ProductResource
     {
+
         $result = $this->service->query()->findOrFail($id);
         return new ProductResource($result);
     }
--- src/Services/Product/Actions/ProductQueryAction.php
+++ src/Services/Product/Actions/ProductQueryAction.php
@@ -3,9 +3,34 @@
 namespace RedJasmine\Product\Services\Product\Actions;
 
 use RedJasmine\Support\Foundation\Service\Actions\ResourceQueryAction;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class ProductQueryAction extends ResourceQueryAction
 {
 
+
+    protected function filters() : array
+    {
+        return [ AllowedFilter::exact('id'),
+                 AllowedFilter::exact('owner_type'),
+                 AllowedFilter::exact('owner_id'),
+                 AllowedFilter::exact('product_type'),
+                 AllowedFilter::exact('shipping_type'),
+                 AllowedFilter::partial('title'),
+                 AllowedFilter::exact('outer_id'),
+                 AllowedFilter::exact('is_multiple_spec'),
+                 AllowedFilter::exact('status'),
+                 AllowedFilter::exact('brand_id'),
+                 AllowedFilter::exact('category_id'),
+                 AllowedFilter::exact('seller_category_id'), ];
+    }
+
+    protected function includes() : array
+    {
+        return [
+            'info', 'skus', 'skus.info', 'brand', 'category', 'sellerCategory', 'series'
+
+        ];
+    }
 
 }
